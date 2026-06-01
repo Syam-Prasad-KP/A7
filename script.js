5002c09fa0c73f557a3515a1ce6322a0f936508a
Syam-KP <psyam2559@gmail.com>
2026-06-01 12:25:55 +0530

    functions added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*const n1 = document.getElementById("n1");
+const n1 = document.getElementById("n1");
 const n2 = document.getElementById("n2");
 const addBtn = document.getElementById("addBtn");
 const subBtn = document.getElementById("subBtn");
